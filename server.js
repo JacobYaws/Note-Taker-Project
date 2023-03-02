244d5cf32bef8471bb66b0e2da8a6e8e4995da83
JacobYaws <Jacob.Yaws@du.edu>
2023-03-02 15:27:55 -0700

    Added in some api routes to the server.js file

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
 const PORT = 3001; 
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('./public'));
 
 app.get('/api/notes', (req,res) => {
     res.json(`${req.method} request received`);
@@ -11,3 +18,28 @@ app.get('/api/notes', (req,res) => {
 
     console.info(`${req.method} request received`);
 })
+
+app.post('/api/notes', (req,res) => {
+    console.info(`${req.method} request received for feedback`);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
+
+app.listen(PORT, function() {
+    console.log(`App listening at http://localhost:${PORT}`);
+})
+
